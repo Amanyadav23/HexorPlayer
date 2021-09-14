9ce2643d1dfbde41427b43763e30826dcf975375
s4shiva <88126858+s4shiva@users.noreply.github.com>
2021-09-14 19:57:50 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from config import BOT_USERNAME
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"💕𝗛𝗲𝗿𝗲 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝗰𝗲 𝗢𝗳 @{BOT_USERNAME}\n✨𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 @𝗔𝗯𝗼𝘂𝘁𝗛𝗲𝘅𝗼𝗿\n🌟𝗗𝗼𝗻𝘁 𝗦𝗽𝗮𝗺 𝗛𝗲𝗿𝗲")
+  await USER.send_message(message.chat.id,"💕𝙷𝚎𝚛𝚎 𝙰𝚜𝚜𝚒𝚜𝚝𝚊𝚗𝚝 𝙾𝚏 @{Amanacf_musicbot}\n✨𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 @shivamdemon\n🌟𝙳𝚘𝚗𝚝 𝚂𝚙𝚊𝚖 𝙷𝚎𝚛𝚎")
   return                        
